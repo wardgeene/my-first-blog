4fe2c1b6006fe4710aaaef77aea6fe002510ce16
Ward Geene <wardgeene@gmail.com>
2015-06-25 22:21:00 +0200

    Added views create/edit blog post inside the site

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
@@ -11,9 +8,9 @@ class Post(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
-def publish(self):
-    self.published_date = timezone.now()
-    self.save()
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
